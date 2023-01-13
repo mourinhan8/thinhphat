5f5503233f2b286bf17a10ccf73d6d52a5fbbc6d
ngocnhan <sbtc.walkingdead@gmail.com>
2023-01-13 13:54:48 +0700

    first commit

--- src/components/Form/Document.jsx
+++ src/components/Form/Document.jsx
@@ -1,6 +1,7 @@
 import { Form, Input, Button, Select, InputNumber, Space, Cascader } from 'antd';
 import { SettingOutlined } from '@ant-design/icons';
 import React from 'react';
+import axios from 'axios';
 
 const { TextArea } = Input;
 const { Option } = Select;
@@ -8,103 +9,112 @@ const { Option } = Select;
 const MyFormItemContext = React.createContext([]);
 
 function toArr(str) {
-    return Array.isArray(str) ? str : [str];
+  return Array.isArray(str) ? str : [str];
 }
 const MyFormItemGroup = ({ prefix, children }) => {
-    const prefixPath = React.useContext(MyFormItemContext);
-    const concatPath = React.useMemo(() => [...prefixPath, ...toArr(prefix)], [prefixPath, prefix]);
-    return <MyFormItemContext.Provider value={concatPath}>{children}</MyFormItemContext.Provider>;
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatPath = React.useMemo(() => [...prefixPath, ...toArr(prefix)], [prefixPath, prefix]);
+  return <MyFormItemContext.Provider value={concatPath}>{children}</MyFormItemContext.Provider>;
 };
 const MyFormItem = ({ name, ...props }) => {
-    const prefixPath = React.useContext(MyFormItemContext);
-    const concatName = name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
-    return <Form.Item name={concatName} {...props} />;
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatName = name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
+  return <Form.Item name={concatName} {...props} />;
 };
 
 const selectAfter = (
-    <Select
-        defaultValue="m2"
-        style={{
-            width: 90,
-        }}
-    >
-        <Option value="m2">m2</Option>
-        <Option value="km2">km2</Option>
-    </Select>
+  <Select
+    defaultValue="m2"
+    style={{
+      width: 90,
+    }}
+  >
+    <Option value="m2">m2</Option>
+    <Option value="km2">km2</Option>
+  </Select>
 );
 
-export default function DocumentForm() {
-    const onFinish = (value) => {
-        console.log(value);
-    };
-    return (
-        <>
-            <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
-                <MyFormItemGroup prefix={['data']}>
-                    <MyFormItem name="wards" label="Xã/phường">
-                        <Input />
-                    </MyFormItem>
-                    <MyFormItem name="document_name" label="Tên">
-                        <Input />
-                    </MyFormItem>
-                    <MyFormItem name="content" label="Nội dung">
-                        <Input />
-                    </MyFormItem>
-                    <MyFormItem name="received_date" label="Ngày nhận">
-                        <Input type='date' />
-                    </MyFormItem>
-                    <MyFormItem name="phone_number" label="Số điện thoại">
-                        <InputNumber style={{ width: '100%' }} />
-                    </MyFormItem>
-                    <MyFormItem name="page_number" label="Số tờ/thửa">
-                        <Input />
-                    </MyFormItem>
-                    <MyFormItem name="current_area" label="Diện tích hiện trạng">
-                        <Space direction="vertical" style={{ width: '100%' }}>
-                            <InputNumber style={{ width: '100%' }} addonAfter={selectAfter} defaultValue={100} />
-                        </Space>
-                    </MyFormItem>
-                    <MyFormItem name="number_certificate" label="Số phát hành GCN">
-                        <Input />
-                    </MyFormItem>
-                    <MyFormItem name="main_person" label="Xử lý chính">
-                        <Select
-                            style={{
-                                width: '100%',
-                            }}
-                            defaultValue=""
-                        >
-                            <Option value="Mến">Mến</Option>
-                        </Select>
-                    </MyFormItem>
-                    <MyFormItem name="sub_person" label="Xử lý nội nghiệp">
-                        <Select
-                            style={{
-                                width: '100%',
-                            }}
-                            defaultValue=""
-                        >
-                            <Option value="Mến">Mến</Option>
-                        </Select>
-                    </MyFormItem>
-                    <MyFormItem name="measure_date" label="Ngày đo">
-                        <Input type='date' />
-                    </MyFormItem>
-                    <MyFormItem name="appraisal_date" label="Ngày thẩm định">
-                        <Input type='date' />
-                    </MyFormItem>
-                    <MyFormItem name="appraiser" label="Cán bộ thẩm định">
-                        <Input />
-                    </MyFormItem>
-                    <MyFormItem name="note" label="Ghi chú">
-                        <TextArea rows={4} />
-                    </MyFormItem>
-                </MyFormItemGroup>
+const API = process.env.API_BACKEND;
 
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form>
-        </>
-    );
+export default function DocumentForm() {
+  const onFinish = async (value) => {
+    console.log(value);
+    const option = {
+      method: 'post',
+      url: `${process.env.API_BACKEND}/document/create`,
+      data: value.data,
+    };
+    const response = await axios(option);
+    console.log(response);
+  };
+  return (
+    <>
+      <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
+        <MyFormItemGroup prefix={['data']}>
+          <MyFormItem name="ward" label="Xã/phường">
+            <Input />
+          </MyFormItem>
+          <MyFormItem name="document_name" label="Tên">
+            <Input />
+          </MyFormItem>
+          <MyFormItem name="content" label="Nội dung">
+            <Input />
+          </MyFormItem>
+          <MyFormItem name="received_date" label="Ngày nhận">
+            <Input type="date" />
+          </MyFormItem>
+          <MyFormItem name="phone_number" label="Số điện thoại">
+            <InputNumber style={{ width: '100%' }} />
+          </MyFormItem>
+          <MyFormItem name="page_number" label="Số tờ/thửa">
+            <Input />
+          </MyFormItem>
+          <MyFormItem name="current_area" label="Diện tích hiện trạng">
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <InputNumber style={{ width: '100%' }} addonAfter={selectAfter} defaultValue={100} />
+            </Space>
+          </MyFormItem>
+          <MyFormItem name="number_certificate" label="Số phát hành GCN">
+            <Input />
+          </MyFormItem>
+          <MyFormItem name="main_person" label="Xử lý chính">
+            <Select
+              style={{
+                width: '100%',
+              }}
+              defaultValue=""
+            >
+              <Option value="Mến">Mến</Option>
+            </Select>
+          </MyFormItem>
+          <MyFormItem name="sub_person" label="Xử lý nội nghiệp">
+            <Select
+              style={{
+                width: '100%',
+              }}
+              defaultValue=""
+            >
+              <Option value="Mến">Mến</Option>
+            </Select>
+          </MyFormItem>
+          <MyFormItem name="measure_date" label="Ngày đo">
+            <Input type="date" />
+          </MyFormItem>
+          <MyFormItem name="appraisal_date" label="Ngày thẩm định">
+            <Input type="date" />
+          </MyFormItem>
+          <MyFormItem name="appraiser" label="Cán bộ thẩm định">
+            <Input />
+          </MyFormItem>
+          <MyFormItem name="note" label="Ghi chú">
+            <TextArea rows={4} />
+          </MyFormItem>
+        </MyFormItemGroup>
+
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 }
--- src/components/table/tableDocument.jsx
+++ src/components/table/tableDocument.jsx
@@ -1,180 +1,105 @@
 import { Pagination } from 'antd';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export const Table = () => {
-    return (
-        <>
-            <div className="overflow-x-auto rounded-lg border border-gray-200 overflow-x-auto">
-                <table className="min-w-full divide-y divide-gray-200 text-sm">
-                    <thead className="bg-gray-100">
-                        <tr>
-                            <th className="bg-gray-100 left-0 insert-y-0 sticky swhitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
-                                <div className="flex items-center gap-2">
-                                    Số TT
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Xã/phường
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Tên
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Nội dung
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Ngày nhận
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Số điện thoại
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Số tờ/thửa
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Diện tích hiện trạng
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Số phát hành GCN
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Xử lý chính
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                Xử lý nội nghiệp
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Ngày đo
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Ngày trình thẩm định
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Ngày thẩm định
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Cán bộ thẩm định
-                                </div>
-                            </th>
-                            <th
-                                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900"
-                            >
-                                <div className="flex items-center gap-2">
-                                    Ghi chú
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
+  const getAllDocument = async () => {
+    try {
+      const option = {
+        method: 'get',
+        url: `${process.env.API_BACKEND}/document/list`,
+      };
+      const response = await axios(option);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllDocument();
+  }, []);
+  return (
+    <>
+      <div className="overflow-x-auto rounded-lg border border-gray-200 overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200 text-sm">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="bg-gray-100 left-0 insert-y-0 sticky swhitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Số TT</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Xã/phường</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Tên</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Nội dung</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Ngày nhận</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Số điện thoại</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Số tờ/thửa</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Diện tích hiện trạng</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Số phát hành GCN</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Xử lý chính</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">Xử lý nội nghiệp</th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Ngày đo</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Ngày trình thẩm định</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Ngày thẩm định</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Cán bộ thẩm định</div>
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                <div className="flex items-center gap-2">Ghi chú</div>
+              </th>
+            </tr>
+          </thead>
 
-                    <tbody className="divide-y divide-gray-200">
-                        <tr>
-                            <td className="sticky inset-y-0 left-0 bg-white px-4 py-2">
-                                #1
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                                #00001
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                John Frusciante
-                            </td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">john@rhcp.com</td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">$783.23</td>
-                            <td className="whitespace-nowrap px-4 py-2">
-                                <strong
-                                    className="rounded bg-red-100 px-3 py-1.5 text-xs font-medium text-red-700"
-                                >
-                                    Cancelled
-                                </strong>
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                1
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                100m2
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                TCDTT
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                Lê Văn Mến
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                Mến Lê
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                20/11/2021
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                10/10/2022
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                10/10/2022
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                Nguyễn Quang Huy
-                            </td>
-                            <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-                                Không
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div className='flex justify-center mt-2'>
-                <Pagination defaultCurrent={1} total={50} />
-            </div>
-        </>
-    );
+          <tbody className="divide-y divide-gray-200">
+            <tr>
+              <td className="sticky inset-y-0 left-0 bg-white px-4 py-2">#1</td>
+              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">#00001</td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">John Frusciante</td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">john@rhcp.com</td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">$783.23</td>
+              <td className="whitespace-nowrap px-4 py-2">
+                <strong className="rounded bg-red-100 px-3 py-1.5 text-xs font-medium text-red-700">Cancelled</strong>
+              </td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">1</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">100m2</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">TCDTT</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">Lê Văn Mến</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">Mến Lê</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">20/11/2021</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">10/10/2022</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">10/10/2022</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">Nguyễn Quang Huy</td>
+              <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">Không</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-center mt-2">
+        <Pagination defaultCurrent={1} total={50} />
+      </div>
+    </>
+  );
 };
